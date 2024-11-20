f240c428b6356ee20325b371a6453173a6c1b27c
goobercoder <vaino.o.hanninen@gmail.com>
2024-11-20 10:51:32 +0000

    modified:   apitest.py

--- apitest.py
+++ apitest.py
@@ -1,43 +1,38 @@
 import requests
-facts= []
+
+facts = []
 fun = {
-  {
   "username": "dauwg",
-  "avatar_url": "https://i.imgur.com/4M34hi2.png",
-  "content": "Text message. Up to 2000 characters.",
+  "avatar_url": "https://static.toiimg.com/photo/114390036/114390036.jpg",
   "embeds": [
     {
       "title": "dauwg",
       "description": "fun dog facts",
       "color": 15258703,
-      "fields": [
-        {
-          "name": "Text",
-          "value": "More text"
-        },
-        {
-          "name": "Even more text",
-          "value": "Yup"
-        },
-        {
-          "name": "Use `\"inline\": true` parameter, if you want to display fields in the same line.",
-          "value": "okay..."
-        },
-        {
-          "name": "Thanks!",
-          "value": "You're welcome :wink:"
-        }
-    ],
+      "fields": []
     }
   ]
 }
-}
+
 discordurl = 'https://discord.com/api/webhooks/1304159442903765103/ggYRi-CJco2HpCQaOGycMGFo55gbfeGP6WRICGE6ebjlAuDJe1s4EIMkpkDy7Fu_5jhz'
 url = 'http://dog-api.kinduff.com/api/facts?number=5'
 r = requests.get(url)
 print(r)
 #print(r.json())
 f = r.json()
+c = 0
 for fact in f["facts"]:
+    c += 1
     facts.append(fact)
-print(facts)
+    z = { 
+        "name": f"dogfact #{c}",
+        "value": fact
+    }
+    fun["embeds"][0]["fields"].append(z)
+
+print(fun)
+
+# Send the payload to Discord webhook
+response = requests.post(discordurl, json=fun)
+print(response.status_code)
+print(response.text)
